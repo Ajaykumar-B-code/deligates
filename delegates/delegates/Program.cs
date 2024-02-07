29b4ea3b6164bd125f58a0eb871f675169487c45
Ajaykumar-B-code <bonagiriajaykumar1@gmail.com>
2024-02-07 15:34:25 +0530

    [Ajay] Add: delegate practice

---
+++ delegates/delegates/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegates
+{
+
+    public delegate void AddDelegate(int x, int y);
+    public delegate string SayDelegate(string str);
+    internal class practice
+    {
+        public void AddNums(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public static string SayHi(string name)
+        {
+            return "Hi " + name;
+        }
+        public static void Main()
+        {
+            practice p = new practice();
+            AddDelegate ap = new AddDelegate(p.AddNums);
+            ap.Invoke(150, 20);
+
+            string str = practice.SayHi("Ajay");
+            Console.WriteLine(str);
+            Console.ReadLine();
+        }
+    }
+}
